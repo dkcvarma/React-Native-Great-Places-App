7e3fb4eaaa099da1ccbf4758fa7f1b30135eb320
Krishna Chaitanya <dkchaitanyavarma06@gmail.com>
2021-03-03 22:42:14 +0530

    Setting up Navigation

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const NewPlaceScreen = props => {
+  return (
+    <View>
+      <Text>NewPlaceScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+NewPlaceScreen.navigationOptions = {
+  headerTitle: 'Add Place',
+};
+
+export default NewPlaceScreen;
